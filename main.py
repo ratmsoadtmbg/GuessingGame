c9818210cec684ae0c4720b72bb6e56cc690c6e7
Travis Tucker <78522574+ratmsoadtmbg@users.noreply.github.com>
2025-07-12 01:36:24 -0600

    Added highscore functionality

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import random
 
+def save_high_score(score, filename="highscore.txt"):
+    try:
+        with open(filename, "w") as f:
+            f.write(str(score))
+    except IOError:
+        print("Error: Unable to save high score.")
+
+def load_high_score(filename="highscore.txt"):
+    try:
+        with open(filename, "r") as f:
+            high_score = int(f.read())
+            return high_score
+    except (FileNotFoundError, ValueError):
+        return 0  # Returns 0 if file doesn't exist or content is invalid
+
 def main():
     number = random.randint(1,9)
     guessed = False
     guesses = []
-    guess_limit = 5
+    guess_limit = 5 # Change number of guesses allowed here
+    high_score = load_high_score()
 
-    print(f"I'm thinking of a number between 0 and 10. Can you guess it in {guess_limit} tries?\n")
+    print(f"I'm thinking of a number between 0 and 10. Can you guess it in {guess_limit} tries?\nThe current lowest guess score is {high_score}.\n")
     
     while guessed == False and guess_limit != 0:
         if guesses:
@@ -26,20 +42,25 @@ def main():
             guesses.append(f"<{guess}")
         else:
             guessed = True
+            
+    score = len(guesses)+1
     
     if guessed == True:
         if not guesses:
+            save_high_score(1)
             print(f"\nCongratulations! You guessed the number in 1 try!")
-        else:    
-            print(f"\nCongratulations! You guessed the number in {len(guesses)+1} tries!")
+        else:
+            if score < high_score:
+                save_high_score(score)    
+            print(f"\nCongratulations! You guessed the number in {score} tries!")
     else:
         print("You have run out of guesses!")
     
     play_again = input("\nWould you like to play again? (Y/N): ").lower()
     if play_again != "y" and play_again != "n":
-        play_again = input('\nSorry but "Y" and "N" are your only options. Would you like to play again?: ')
+        play_again = input('\nSorry but "Y" and "N" are your only options. Would you like to play again?: ') # Gives second chance to put a valid response
     if play_again != "y" and play_again != "n":
-        print("\nOkay, wise guy, have it your way.")
+        print("\nOkay, wise guy, have it your way.") # Boots wiley players who won't respond properly
     if play_again == "y":
         main()
     if play_again == "n":

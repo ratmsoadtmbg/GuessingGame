543bb45d6c6ede821d84c1cd9d2fa9d5f3e6b870
Travis Tucker <78522574+ratmsoadtmbg@users.noreply.github.com>
2025-07-11 18:22:04 -0600

    Added fail state for player

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ def main():
     number = random.randint(1,9)
     guessed = False
     guesses = []
+    guess_limit = 5
 
-    print("I'm thinking of a number between 0 and 10. Can you guess it?\n")
+    print(f"I'm thinking of a number between 0 and 10. Can you guess it in {guess_limit} tries?\n")
     
-    while guessed == False:
+    while guessed == False and guess_limit != 0:
         if guesses:
             print(guesses)
         try:
@@ -16,18 +17,24 @@ def main():
             print("I know you think you're clever, but please try to stick to simple numbers here.\n")
             continue
         if guess < number:
-            print("\nToo low! Try again!")
+            guess_limit -= 1
+            print(f"\nToo low! You have {guess_limit} attempts left.")
             guesses.append(f">{guess}")
         elif guess > number:
-            print("\nToo high! Try again!")
+            guess_limit -= 1
+            print(f"\nToo high! You have {guess_limit} attempts left.")
             guesses.append(f"<{guess}")
         else:
             guessed = True
     
-    if not guesses:
-        print(f"\nCongratulations! You guessed the number in 1 try!")
-    else:    
-        print(f"\nCongratulations! You guessed the number in {len(guesses)+1} tries!")
+    if guessed == True:
+        if not guesses:
+            print(f"\nCongratulations! You guessed the number in 1 try!")
+        else:    
+            print(f"\nCongratulations! You guessed the number in {len(guesses)+1} tries!")
+    else:
+        print("You have run out of guesses!")
+    
     play_again = input("\nWould you like to play again? (Y/N): ").lower()
     if play_again != "y" and play_again != "n":
         play_again = input('\nSorry but "Y" and "N" are your only options. Would you like to play again?: ')
